598dc30c6cf724410791fc44a0683e0986b0e9bc
Nastia9 <91213799+Nastia9@users.noreply.github.com>
2021-10-27 22:35:24 +0300

    Add files via upload

---
+++ lab_04_7.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double xp, xk, x, dx, eps, Pi = 3.1415926535, a=0, R=0, S=0;
+    int n=0;
+    cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << "  |"
+         << setw(10) << "atan(x) " << "  |"
+         << setw(7)  << "S"      << "    |"
+         << setw(5)  << "n"      << "    |"
+         << endl;
+    cout << "-------------------------------------------------" << endl;     
+
+    x = xp;
+   
+    while ( x <= xk) 
+    {  
+        n = 0;
+        a = -1. / x;
+        S = a;
+         do { 
+             n++;
+             R =  (1. - 2. * n) / ((2. * n * x * x) + (x * x));
+             a *= R;
+             S += a;
+        } while (abs(a)>=eps);
+         S += (Pi / 2. ) ;
+        cout << "|" << setw(7) << setprecision(2) << x << "  |"
+			<< setw(10) << setprecision(5) << atan(x) << "  |"
+			<< setw(10) << setprecision(5) << S << "  |"
+			<< setw(5) << n << "  |"
+			<< endl; 
+         x+= dx;
+            
+    }
+    cout << "-------------------------------------------------" << endl;
+    return 0;
+ }
